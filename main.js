08443d783e461afb5678e2687a79edf74285f382
nadiawaseem <161966596+Nadiawaseem@users.noreply.github.com>
2024-06-08 16:38:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+let names = ["Hammad ali ", "Shebaz", " Naveed"];
+// for loop
+//  1 inialization 2 condition 3 icreament/decrement
+for (let i = 0; i < names.length;) {
+    console.log(names[i]);
+}
+//i = 0
+// i < variable.lenght
+//icrement i =0 , 0+1 = 1 , 1+1= 2
---
+++ main.ts
@@ -0,0 +1,14 @@
+  let names :string[] = ["Hammad ali " ,"Shebaz" ," Naveed"];
+  // for loop
+  
+
+  //  1 inialization 2 condition 3 icreament/decrement
+
+
+  
+    for (let i = 0; i < names.length ;) {
+        console.log(names[i]);
+    }
+    //i = 0
+    // i < variable.lenght
+    //icrement i =0 , 0+1 = 1 , 1+1= 2
